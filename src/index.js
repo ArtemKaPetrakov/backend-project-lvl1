bc02a17e072d678d595e1205c5f76c8f5ed43837
Artem Petrakov <3885604@mail.ru>
2022-08-16 19:39:04 +0300

    add logic to index.js, question and answer in even.js, brain-even.js only import

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
+export default (description, questionAndCorrectAnswer) => {
+  
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name?: ');
-  return console.log(`Hello, ${userName}!`);
-};
+  console.log(`Hello, ${userName}!`);
 
-// приветствие для Игры
+  console.log(description); // Описание игры, само описание в игре
+
+  let count = 0;
+
+  do {
+    const [question, correctAnswer] = questionAndCorrectAnswer();
+
+    console.log(`Question: ${question}`); // задаем вопрос, вопрос в игре
+
+    const userAnswer = readlineSync.question('');
+
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+      count += 1;
+    }
+    if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
+      break;
+    }
+    if (count === 3) {
+      console.log(`Congratulations, ${userName}!`);
+    }
+  } while (count < 3);
+};
