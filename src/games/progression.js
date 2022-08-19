2e33a90fe61cde0ad67119f163cd140fca939997
Artem Petrakov <3885604@mail.ru>
2022-08-19 11:41:14 +0500

    fix brain-progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,11 +2,29 @@ import _ from 'lodash';
 
 import gameStart from '../index.js';
 
-const description = 'Find the greatest common divisor of given numbers.';
+const description = 'What number is missing in the progression?';
+const progressionLength = _.random(5, 10); // длинна прогрессии от 5 до 10
+const getProgression = () => { // формируем прогрессию
+  let firstNumber = _.random(0, 15); // первый элемент
+  const step = _.random(1, 5); // шаг прогрессии
+  const progression = []; // массив значений прогрессии
+
+  for (let i = 0; i < progressionLength; i += 1) {
+    progression.push(firstNumber); // пушаем первый элеменет
+    firstNumber += step; // меняем на шаг прогрессии
+  }
+  return progression;
+};
 
 const questionAndCorrectAnswer = () => {
+  const progression = getProgression(); // прогрессия
+  const indexOfHiddenElement = _.random(0, progressionLength);
+  // получаем индекс спрятанного элемента от 0 до длинны прогрессии
+  const correctAnswer = String(progression[indexOfHiddenElement]); // правильный ответ
+  progression[indexOfHiddenElement] = '..'; // менянем на '..'
+  const question = progression.join(' '); // выводим вопрос (прогрессию с спрятанным элементом)
 
-  return [question, String(correctAnswer)];
+  return [question, correctAnswer];
 };
 
 export default () => {
