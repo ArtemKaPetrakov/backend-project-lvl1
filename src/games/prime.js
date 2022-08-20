116dbacae548fe7086160c5ed8c93a8a4665fd95
Artem Petrakov <3885604@mail.ru>
2022-08-20 13:49:58 +0500

    fix brain-prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,12 +1,25 @@
+/* eslint-disable no-unreachable-loop */
 import _ from 'lodash';
 
 import gameStart from '../index.js';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const questionAndCorrectAnswer = () => {
+const isPrime = (number) => {
+  for (let i = 2; i < number / 2; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
 
-  return [question, String(correctAnswer)];
+const questionAndCorrectAnswer = () => {
+  const question = _.random(0, 20);
+
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+
+  return [question, correctAnswer];
 };
 
 export default () => {
