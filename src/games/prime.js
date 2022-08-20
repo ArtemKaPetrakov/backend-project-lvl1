ad145b918ab93571f31018ae1d2edcf8e2a17b5c
Artem Petrakov <3885604@mail.ru>
2022-08-20 15:10:13 +0500

    fix brain-prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,7 +6,7 @@ import gameStart from '../index.js';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
-  for (let i = 2; i < number / 2; i += 1) {
+  for (let i = 2; i < number; i += 1) {
     if (number % i === 0) {
       return false;
     }
